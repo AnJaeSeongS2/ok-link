27745f138b53589bc2a5c6ce7d0a47c0dbea549b
Jaeseong An <jaeseong@Jaeseongs-MacBook-Pro.local>
2021-04-25 12:58:43 +0900

    [trivial] first commit.

---
+++ src/main/java/org/sonan/oklink/OkLinkApplication.java
@@ -0,0 +1,13 @@
+package org.sonan.oklink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OkLinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sonan/oklink/OkLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sonan.oklink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
